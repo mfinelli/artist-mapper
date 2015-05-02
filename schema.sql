19c8a2b63c93c5ac24e82226bca9d3244d04dcad
Mario Finelli <mfinelli@umd.edu>
2015-05-02 09:08:25 -0400

    Add new schema for finding tweets

--- schema.sql
+++ schema.sql
@@ -22,10 +22,33 @@ CREATE TABLE `tracks` (
 
 DROP TABLE IF EXISTS `similars`;
 CREATE TABLE `similars` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `track_1` int(10) unsigned NOT NULL,
   `track_2` int(10) unsigned NOT NULL,
+  `tracks_processed` tinyint(1) unsigned DEFAULT 0,
+  `artists_processed` tinyint(1) unsigned DEFAULT 0,
+  `track_one_tweets` int(10) unsigned DEFAULT 0,
+  `track_two_tweets_by_same_users` int(10) unsigned DEFAULT 0,
+  `track_two_not_tweeted_by_same_users` int(10) unsigned DEFAULT 0,
+  `track_one_artist_tweets` int(10) unsigned DEFAULT 0,
+  `track_two_artist_tweets_by_same_users` int(10) unsigned DEFAULT 0,
+  `track_two_artist_not_tweeted_by_same_users` int(10) unsigned DEFAULT 0,
+  PRIMARY KEY (`id`),
   KEY `track_1` (`track_1`),
   KEY `track_2` (`track_2`),
   CONSTRAINT `similars_ibfk_1` FOREIGN KEY (`track_1`) REFERENCES `tracks` (`id`),
   CONSTRAINT `similars_ibfk_2` FOREIGN KEY (`track_2`) REFERENCES `tracks` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+DROP TABLE IF EXISTS `tweets`;
+CREATE TABLE `tweets` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `similar` int(10) unsigned NOT NULL,
+  `user` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
+  `tweet` varchar(160) COLLATE utf8_unicode_ci NOT NULL,
+  `track` tinyint(1) unsigned DEFAULT 0, # 0 = artist, 1 = track 1, 2 = track 2
+  `artist` tinyint(1) unsigned DEFAULT 0, # 0 = track, 1 = artist
+  PRIMARY KEY (`id`),
+  KEY `similar` (`similar`),
+  CONSTRAINT `tweets_ibfk_1` FOREIGN KEY (`similar`) REFERENCES `similars` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
