fecd32f7c29066a057d88d29955143630d28f9d3
Mario Finelli <mfinelli@umd.edu>
2015-05-02 19:59:48 -0400

    Update schema after more experimentation
    It isn't realistic to perform all of the tests that we prepared for
    becuase Twitter's rate limiting is pretty extreme.

--- schema.sql
+++ schema.sql
@@ -26,29 +26,17 @@ CREATE TABLE `similars` (
   `track_1` int(10) unsigned NOT NULL,
   `track_2` int(10) unsigned NOT NULL,
   `tracks_processed` tinyint(1) unsigned DEFAULT 0,
-  `artists_processed` tinyint(1) unsigned DEFAULT 0,
-  `track_one_tweets` int(10) unsigned DEFAULT 0,
-  `track_two_tweets_by_same_users` int(10) unsigned DEFAULT 0,
-  `track_two_not_tweeted_by_same_users` int(10) unsigned DEFAULT 0,
-  `track_one_artist_tweets` int(10) unsigned DEFAULT 0,
-  `track_two_artist_tweets_by_same_users` int(10) unsigned DEFAULT 0,
-  `track_two_artist_not_tweeted_by_same_users` int(10) unsigned DEFAULT 0,
-  PRIMARY KEY (`id`),
-  KEY `track_1` (`track_1`),
-  KEY `track_2` (`track_2`),
-  CONSTRAINT `similars_ibfk_1` FOREIGN KEY (`track_1`) REFERENCES `tracks` (`id`),
-  CONSTRAINT `similars_ibfk_2` FOREIGN KEY (`track_2`) REFERENCES `tracks` (`id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-DROP TABLE IF EXISTS `tweets`;
-CREATE TABLE `tweets` (
+DROP TABLE IF EXISTS `matches`;
+CREATE TABLE `matches` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `similar` int(10) unsigned NOT NULL,
   `user` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
-  `tweet` varchar(160) COLLATE utf8_unicode_ci NOT NULL,
-  `track` tinyint(1) unsigned DEFAULT 0, # 0 = artist, 1 = track 1, 2 = track 2
-  `artist` tinyint(1) unsigned DEFAULT 0, # 0 = track, 1 = artist
+  `tweet_1` varchar(160) COLLATE utf8_unicode_ci NOT NULL,
+  `tweet_2` varchar(160) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `similar` (`similar`),
-  CONSTRAINT `tweets_ibfk_1` FOREIGN KEY (`similar`) REFERENCES `similars` (`id`)
+  CONSTRAINT `tweets_ibfk_3` FOREIGN KEY (`similar`) REFERENCES `similars` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
